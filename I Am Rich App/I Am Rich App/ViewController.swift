7f175556b99fee99fe262e9f385b6a347899e5d6
manan gandhi <manangandhi@manans-Mac-mini.local>
2023-05-09 17:00:58 -0400

    added Xylophone

---
+++ I Am Rich App/I Am Rich App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich App
+//
+//  Created by manan gandhi on 5/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am Rich App/I Am Rich AppUITests/I_Am_Rich_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_Am_Rich_AppUITestsLaunchTests.swift
+//  I Am Rich AppUITests
+//
+//  Created by manan gandhi on 5/3/23.
+//
+
+import XCTest
+
+final class I_Am_Rich_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
